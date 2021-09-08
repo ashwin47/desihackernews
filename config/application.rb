513d824560e3b823c72b7e5c24b8a300c2a72d04
Ashwin M <thatsashwin@gmail.com>
2021-09-08 07:47:58 +0530

    change timezone

--- config/application.rb
+++ config/application.rb
@@ -37,7 +37,7 @@ module Lobsters
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Asia/Calcutta'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
@@ -78,11 +78,11 @@ class << Rails.application
   end
 
   def open_signups?
-    Rails.application.credentials.config[:OPEN_SIGNUPS] == "true"
+    Rails.application.credentials.config[:OPEN_SIGNUPS]
   end
 
   def domain
-    Rails.application.credentials.DOMAIN || "lvh.me:3000"
+    Rails.application.credentials.config[:DOMAIN] || "lvh.me:3000"
   end
 
   def name
