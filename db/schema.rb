74e8cadfb8000b6e2a2aa6b768373afb477f25bc
Ashwin M <thatsashwin@gmail.com>
2021-09-08 18:31:56 +0530

    changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_28_015742) do
+ActiveRecord::Schema.define(version: 2020_08_11_221324) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "category"
