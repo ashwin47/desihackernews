1ad713e286ae2ec7f1b7cface32413fecd327a07
Ashwin M <thatsashwin@gmail.com>
2021-09-11 16:00:39 +0530

    keybase disable

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,6 +58,3 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.hosts << "lvh.me"
 end
-
-Keybase.DOMAIN = Rails.application.domain
-Keybase.BASE_URL = ENV.fetch('KEYBASE_BASE_URL') { 'https://keybase.io' }
